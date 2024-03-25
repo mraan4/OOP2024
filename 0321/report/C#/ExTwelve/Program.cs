4d12fcc1dddc7c702c5b6ac8a624b9577ff05fed
mraan4 <76992597+mraan4@users.noreply.github.com>
2024-03-25 09:48:01 +0900

    Add files via upload

---
+++ 0321/report/C#/ExTwelve/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExTwelve
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("연산>>");
+            int op1 = Convert.ToInt32(Console.ReadLine());
+            string op = Console.ReadLine();
+            int op2 = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            if (op.Equals("+"))
+                result = op1 + op2;
+            else if (op.Equals("-"))
+                result = op1 - op2;
+            else if (op.Equals("*"))
+                result = op1 * op2;
+            else if (op.Equals("/"))
+            {
+                if (op2 == 0)
+                {
+                    Console.WriteLine("0으로 나눌 수 없습니다.");
+                    return;
+                }
+                else
+                    result = op1 / op2;
+            }
+            else
+            {
+                Console.WriteLine("사칙연산이 아닙니다.");
+                return;
+            }
+            Console.WriteLine(op1 + op + op2 + "의 계산 결과는 " + result);
+        }
+    }
+
+}
---
+++ 0321/report/C#/ExTwelveTwo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExTwelveTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("연산>>");
+            int op1 = Convert.ToInt32(Console.ReadLine());
+            string op = Console.ReadLine();
+            int op2 = Convert.ToInt32(Console.ReadLine());
+
+            switch (op)
+            {
+                case "+":
+                    Console.WriteLine(op1 + "" + op + "" + op2 + "의 계산 결과는 " + (op1 + op2));
+                    break;
+                case "-":
+                    Console.WriteLine(op1 + "" + op + "" + op2 + "의 계산 결과는 " + (op1 - op2));
+                    break;
+                case "*":
+                    Console.WriteLine(op1 + "" + op + "" + op2 + "의 계산 결과는 " + (op1 * op2));
+                    break;
+                case "/":
+                    if (op2 == 0)
+                    {
+                        Console.WriteLine("0으로 나눌 수 없습니다.");
+                        break;
+                    }
+                    Console.WriteLine(op1 + "" + op + "" + op2 + "의 계산 결과는 " + (op1 / op2));
+                    break;
+            }
+        }
+    }
+}
